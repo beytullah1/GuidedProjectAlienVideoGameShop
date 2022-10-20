0d36388fe19717c214ff398281f5d420323f3c44
Beytullah Akkuş <65204184+beytullah1@users.noreply.github.com>
2022-10-20 22:24:49 +0300

    qq

---
+++ AlienShop/Assets/Scripts/a.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a : MonoBehaviour
+{
+    int x, y;
+    public int Adsa()
+    {
+        int b = x + y;
+        return b;
+    }
+    private void Start()
+    {
+        Adsa();
+    }
+}
